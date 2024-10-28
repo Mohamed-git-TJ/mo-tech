8e2188a69e5e821ca9b3f0c288363da99f4d090f
Mohamed <mohamed02tj@gmail.com>
2024-10-28 01:33:51 +0000

    NavBar improvements and toggle button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.css";
 interface NavBarProps {
   companyName: string;
   imgSrcPath: string;
@@ -5,17 +6,30 @@ interface NavBarProps {
 
 function NavBar({ companyName, imgSrcPath }: NavBarProps) {
   return (
-    <nav className="navbar navbar-dark bg-dark shadow">
-      <a className="navbar-brand" href="#">
-        <img
-          src={imgSrcPath}
-          width="30"
-          height="30"
-          className="d-inline-block align-center"
-          alt=""
-        />
-        <span className="fw-bolder fs-4">{companyName}</span>
-      </a>
+    <nav className="navbar navbar expand-md navbar-dark bg-dark shadow">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src={imgSrcPath}
+            width="30"
+            height="30"
+            className="d-inline-block align-center"
+            alt=""
+          />
+          <span className="fw-bolder fs-4">{companyName}</span>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
     </nav>
   );
 }
