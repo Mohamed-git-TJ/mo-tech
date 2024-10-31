f22582b2ecba1d130d1059c235608843fe0baa6a
Mohamed <mohamed02tj@gmail.com>
2024-10-31 02:56:10 +0000

    Quick NavBar Improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
+import imagePath from "./assets/eclipse.png";
+
 // (importar logo en el futuro) import imagePath from ''
 function App() {
   return (
     <div>
-      <NavBar companyName="MoTech" imgSrcPath="" />
+      <NavBar companyName="MoTech" imgSrcPath={imagePath} />
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
+
 interface NavBarProps {
   companyName: string;
   imgSrcPath: string;
@@ -77,17 +78,6 @@ function NavBar({ companyName, imgSrcPath }: NavBarProps) {
               <a className="nav-link disabled">Disabled</a>
             </li>
           </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
