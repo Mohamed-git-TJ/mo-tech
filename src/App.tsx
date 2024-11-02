b5a416655bb83ab9695df70430fc4ef16ecc64c3
Mohamed <mohamed02tj@gmail.com>
2024-11-02 07:19:24 +0000

    More NavBar Improvements

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import NavBar from "./components/NavBar";
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import imagePath from "./assets/eclipse.png";
 
 // (importar logo en el futuro) import imagePath from ''
 function App() {
+  let items = ["Home", "Projects", "Information", "Contact Me"];
   return (
     <div>
-      <NavBar companyName="MoTech" imgSrcPath={imagePath} />
+      <NavBar companyName="MoTech" imgSrcPath={imagePath} navItems={items} />
     </div>
   );
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,15 @@
 import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
 interface NavBarProps {
   companyName: string;
   imgSrcPath: string;
+  navItems: string[];
 }
 
-function NavBar({ companyName, imgSrcPath }: NavBarProps) {
+function NavBar({ companyName, imgSrcPath, navItems }: NavBarProps) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark shadow">
       <div className="container-fluid">
@@ -32,52 +34,33 @@ function NavBar({ companyName, imgSrcPath }: NavBarProps) {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
+        <div
+          className="collapse 
+          navbar-collapse 
+          align-items-start 
+          flex-column 
+          flex-md-row"
+          id="navbarSupportedContent"
+        >
+          <ul className="navbar-nav me-auto mb-2 mb-md-1">
+            {navItems.map((items, index) => (
+              <li
+                key={items}
+                className="nav-item"
+                onClick={() => setSelectedIndex(index)}
               >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
-            </li>
+                <a
+                  className={
+                    selectedIndex == index
+                      ? "nav-link active fw-bold"
+                      : "nav-link"
+                  }
+                  href="#"
+                >
+                  {items}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
