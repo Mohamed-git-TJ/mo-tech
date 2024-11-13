7b3de00003de228afb55c1ca0906213bf586c6b0
Mohamed <mohamed02tj@gmail.com>
2024-11-13 11:35:37 +0000

    More improvements and changes in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,39 @@
 function Footer() {
   return (
-    <div className="card fixed-bottom rounded-0 bg-dark shadow ">
+    <div className="main-footer fixed-bottom bg-dark shadow ">
       <div className="card-body text-light">
-        <h5 className="card-title">Footer</h5>
-        <p className="card-text">
-          Info (Information about me, how to contact me, links to linkedin and
-          github etc)
-        </p>
+        <h3 className="card-title mb-2">Footer</h3>
+        <div className="row">
+          <div className="col-md-3 col-sm-6">
+            <h5>My details</h5>
+            <ul className="list-unstyled">
+              <li>Mohamed Triki Jouhrati</li>
+              <li>mohamed02tj@gmail.com</li>
+              <li>07443405531</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <h5>My details</h5>
+            <ul className="list-unstyled">
+              <li>Mohamed Triki Jouhrati</li>
+              <li>mohamed02tj@gmail.com</li>
+              <li>07443405531</li>
+            </ul>
+          </div>
+          <div className="col-md-3 col-sm-6">
+            <h5>My details</h5>
+            <ul className="list-unstyled">
+              <li>Mohamed Triki Jouhrati</li>
+              <li>mohamed02tj@gmail.com</li>
+              <li>07443405531</li>
+            </ul>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p className="small text-center">
+            Esto es de mi propiedad (anadir fecha con typescript)
+          </p>
+        </div>
       </div>
     </div>
   );
