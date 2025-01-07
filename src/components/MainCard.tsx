64b4baf45f155a5a689e7a52218fed8d95f04afa
Mohamed <mohamed02tj@gmail.com>
2025-01-07 11:55:40 +0000

    Flags image implementation in Home Page

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -5,9 +5,10 @@ import "../scssFiles/main.scss";
 interface MainCardProps {
   imgSrcPath: string;
   imgSrcPath1: string;
+  flagsPath: string;
 }
 
-function MainCard({ imgSrcPath, imgSrcPath1 }: MainCardProps) {
+function MainCard({ imgSrcPath, imgSrcPath1, flagsPath }: MainCardProps) {
   return (
     <div className="card mx-2 my-2 bg-info">
       <h4 className="card-header bg-success text-white text-center">
@@ -25,7 +26,7 @@ function MainCard({ imgSrcPath, imgSrcPath1 }: MainCardProps) {
           />
         </div>
         <div className="flex-grow-1 ms-3">
-          <h5 className="card-title">My story</h5>
+          <h5 className="card-title">I'm Mohamed!</h5>
           <p className="card-text">
             Of Moroccan/Spanish origin and graduated in Computer Science at the
             University of Chester, Mohamed is a developer who came to the United
@@ -36,6 +37,15 @@ function MainCard({ imgSrcPath, imgSrcPath1 }: MainCardProps) {
             which he plans to innovate and give another point of view regarding
             problem solving.
           </p>
+        </div>
+        <div className="flex-shrink-0">
+          <img
+            src={flagsPath}
+            width="220"
+            height="220"
+            className="img-fluid  mt-4"
+            alt=""
+          />
         </div>
       </div>
       <div className="card-body d-flex">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
 import imagePath from "../assets/eclipse.png";
 import imagePath1 from "../assets/mohamedColegio.jpg";
+import flagsPath from "../assets/flags.png";
 import MainCard from "../components/MainCard";
 
 function Home() {
   //const [selectedIndex, setSelectedIndex] = useState(-1);
-  return <MainCard imgSrcPath={imagePath1} imgSrcPath1={imagePath} />;
+  return (
+    <MainCard
+      imgSrcPath={imagePath1}
+      imgSrcPath1={imagePath}
+      flagsPath={flagsPath}
+    />
+  );
 }
 
 export default Home;
