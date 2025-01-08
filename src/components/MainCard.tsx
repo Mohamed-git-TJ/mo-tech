719856e8dce3a0b84df71a91db5d1a1acf87108b
Mohamed <mohamed02tj@gmail.com>
2025-01-08 12:07:19 +0000

    skills and services paragraph implementation

--- src/components/MainCard.tsx
+++ src/components/MainCard.tsx
@@ -10,12 +10,12 @@ interface MainCardProps {
 
 function MainCard({ imgSrcPath, imgSrcPath1, flagsPath }: MainCardProps) {
   return (
-    <div className="card mx-2 my-2 bg-info">
+    <div className="card mx-2 mt-3 mb-5 bg-info">
       <h4 className="card-header bg-success text-white text-center">
         Welcome to MoTech
       </h4>
 
-      <div className="card-body d-flex ">
+      <div className="card-body d-flex border-bottom border-2">
         <div className="flex-shrink-0">
           <img
             src={imgSrcPath}
@@ -49,21 +49,42 @@ function MainCard({ imgSrcPath, imgSrcPath1, flagsPath }: MainCardProps) {
         </div>
       </div>
       <div className="card-body d-flex">
+        <div className="flex-grow-1 ms-3">
+          <h5 className="card-title">Skills and Services</h5>
+          <p className="card-text">
+            I specialize mainly in web development and programming in different
+            languages. I have experience in development using React accompanied
+            by TypeScript and HTML along with Bootstrap as a library for CSS. On
+            the other hand, I also have different projects in various
+            programming languages ​​such as Java, C++ and Python, although the
+            latter is the one I am most familiar with. <br /> <br />
+            The services I offer range from developing web pages for different
+            clients, helping designs and ideas to become real in a simple and
+            functional way, to programming different software depending on the
+            functions and features required for the development. <br /> <br />
+            <h6 className="fw-bold">
+              Tools, software and programming languages ​​I use.
+            </h6>
+            <p className="card-text">
+              <br />
+              VSCode <br />
+              GitHub
+              <br />
+              <br /> Typescript (React Dev)
+              <br /> Java
+              <br /> C++
+              <br /> Python
+            </p>
+          </p>
+        </div>
         <div className="flex-shrink-0">
           <img
             src={imgSrcPath1}
-            width="120"
-            height="80"
+            width="200"
+            height="200"
             className="img-fluid float-left rounded d-inline-block align-center"
             alt=""
           />
-        </div>
-        <div className="flex-grow-1 ms-3">
-          <h5 className="card-title">My story</h5>
-          <p className="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
-          </p>
         </div>
       </div>
     </div>
