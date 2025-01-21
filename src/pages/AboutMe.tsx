c928156417e8eeb115a95c29caa15d735b242fa6
Mohamed <mohamed02tj@gmail.com>
2025-01-21 11:46:50 +0000

    card implementation in about tab (adding relevant information)

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -1,6 +1,47 @@
 function AboutMe() {
   //const [selectedIndex, setSelectedIndex] = useState(-1);
-  return <h1>About Me</h1>;
+  return (
+    <div className="card mx-2 mt-3 mb-5 bg-info">
+      <h3 className="card-header bg-success text-white text-center">
+        About Mo'Tech
+      </h3>
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="card-body">
+            <h3 className="card-header bg-success text-white text-center rounded-0 border border-4">
+              General Information
+            </h3>
+            <h4 className="card-title text-center mt-2 fw-bolder">Education</h4>
+            <p className="card-text text-center">
+              <h5 className="card-title text-center mt-4">
+                Computer Science Bsc
+              </h5>
+              University of Chester (2020-2024)
+              <br /> <br />
+              <h4 className="card-title text-center mt-2 fw-bolder">
+                Languages
+              </h4>
+              <h5 className="card-title text-center mt-4">Spanish</h5>
+              <h5 className="card-title text-center mt-4">Arabic (Morocco)</h5>
+            </p>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="card-body ">
+            <h3 className="card-header bg-success text-white text-center rounded-0 border border-4">
+              Contact Me
+            </h3>
+            <h4 className="card-title text-center mt-2 fw-bolder">
+              Send me a message
+            </h4>
+            <p className="card-text">
+              This space would be for the future contact form <br /> <br />
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutMe;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,6 +1,13 @@
 function Projects() {
   //const [selectedIndex, setSelectedIndex] = useState(-1);
-  return <h1>Projects</h1>;
+  return (
+    <div className="card mx-2 mt-3 mb-5 bg-info">
+      <h3 className="card-header bg-success text-white text-center">
+        Developed Projects
+      </h3>
+      <div className="card-body"></div>
+    </div>
+  );
 }
 
 export default Projects;
